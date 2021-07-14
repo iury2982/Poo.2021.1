4013cf4eb64dd8d321bf47e01082c8f6758306e9
Iury2982 <iuryoliveiraqueiroz@alu.ufc.br>
2021-07-14 11:40:01 -0300

    Atualizando

---
+++ Quadrado dos numeros/src/Quadrado.java
@@ -0,0 +1,12 @@
+import java.util.Array;
+
+public class Quadrado {
+    public static void main(String[] args) throws Exception {
+        int[] valores = new int[5];
+        int resultado = 0;
+        for(int i = 0; i<valores.length; i++){
+            resultado = (valores[i]*valores[i]);
+        }         
+    System.out.println(resultado);    
+    }
+}
