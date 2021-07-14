c6ef0cd12e7549909583ca94b90eaef813898b23
Iury2982 <iuryoliveiraqueiroz@alu.ufc.br>
2021-07-14 11:12:38 -0300

    Atualização

---
+++ Soma dos numeros/src/Media.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Media {
+    public static void main(String[] args) throws Exception {
+        Scanner scanner = new Scanner (System.in);
+        System.out.println("Digite os numeros");
+
+        double soma = 0;
+        double Media = 0;
+        int div = 0;
+        while(true){
+            int a = scanner.nextInt();
+            if(a == -1)
+                break;
+            soma += a;
+            div ++;
+        }
+        scanner.close();
+        Media = soma/div;
+        System.out.println(Media);
+    }
+}
--- Soma dos numeros/src/Soma.java
+++
@@ -1,5 +0,0 @@
-public class Soma {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Primeiro Teste");
-    }
-}
